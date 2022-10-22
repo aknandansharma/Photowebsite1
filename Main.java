3a90e57e854683651735443eef5bc6c8a8837f86
aknandansharma <aknandankumar@outlook.com>
2022-10-22 22:36:01 +0530

    final commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+        public static void printPair(int num[]) {
+            int tp = 0;
+            for(int i=0;i<num.length;i++){
+                int curr = num[i];
+                for(int j=i+1;j<num.length;j++){
+                    System.out.print("(" + curr + "," + num[j] + ")");
+                    tp++;
+                }
+                System.out.println();
+            }
+            System.out.println(tp);
+        }
+    public static void main(String[] args) {
+        int num[] = {1,2,3,4,5,6,7,8,9};
+        printPair(num);
+    }
+}                                                                                   
+  
